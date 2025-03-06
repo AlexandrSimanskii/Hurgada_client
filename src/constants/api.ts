651e15ff4ce7f189816dcf0bb2b0868138e4fd86
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2025-03-06 10:57:21 +0200

    change api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,7 @@
-export const BASE_URL:string = "http://localhost:3004/api/";
-export const NIGHTS :string = `${BASE_URL}nights/get`
-export const FOODS :string = `${BASE_URL}foods/get`
-export const EXCURSION :string = `${BASE_URL}excursions/get`
-export const NIGHTS_CATEGORIES :string = `${BASE_URL}nights/categories`
-export const FOODS_CATEGORIES :string = `${BASE_URL}foods/categories`
-export const EXCURSION_CATEGORIES :string = `${BASE_URL}excursions/categories`
+export const BASE_URL: string = 'https://hurgada-server.vercel.app/api/'
+export const NIGHTS: string = `${BASE_URL}nights/get`
+export const FOODS: string = `${BASE_URL}foods/get`
+export const EXCURSION: string = `${BASE_URL}excursions/get`
+export const NIGHTS_CATEGORIES: string = `${BASE_URL}nights/categories`
+export const FOODS_CATEGORIES: string = `${BASE_URL}foods/categories`
+export const EXCURSION_CATEGORIES: string = `${BASE_URL}excursions/categories`
